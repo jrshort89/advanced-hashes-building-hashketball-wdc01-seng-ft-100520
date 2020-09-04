e4ae5ab7282946a48bd22f990db8c4da803d820b
Jacob Short <jrshort89@gmail.com>
2020-09-04 13:17:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,12 +4,29 @@ def game_hash
     :team_name => "Brooklyn Nets",
     :colors => "Black, White",
     :players => [{
-      :player_name => "",
-      
+      :player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1,
     }]
   }, :away => {
-    :team_name => "",
-    :colors => "",
-    :players => []
+    :team_name => "Charlotte Hornets",
+    :colors => "Turquoise, Purple",
+    :players => [{
+      :player_name => "Jeff Adrien",
+      :number => 4,
+      :shoe => 18,
+      :points => 10,
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2,
+    }]
   }}
 end
