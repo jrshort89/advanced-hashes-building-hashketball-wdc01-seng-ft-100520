59fa7f150b49bb6cb8ee3d1b3613ec8b7fd64c39
Jacob Short <jrshort89@gmail.com>
2020-09-04 13:16:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,15 @@
 # Write your code here!
+def game_hash
+  {:home => {
+    :team_name => "Brooklyn Nets",
+    :colors => "Black, White",
+    :players => [{
+      :player_name => "",
+      
+    }]
+  }, :away => {
+    :team_name => "",
+    :colors => "",
+    :players => []
+  }}
+end
